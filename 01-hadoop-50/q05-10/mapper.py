c8e61d9d6c7240a27240d9928d7c26f2ae19798f
dazuluag <dazuluag@unal.edu.co>
2019-12-19 12:53:56 -0500

    Tarea 01-hadoop-50 q05-10

--- 01-hadoop-50/q05-10/mapper.py
+++ 01-hadoop-50/q05-10/mapper.py
@@ -2,3 +2,6 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+        print("{}\t1".format(line.split("   ")[1].split("-")[1]))
