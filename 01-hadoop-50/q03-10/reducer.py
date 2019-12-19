a57a08d8e94c9001e000cb91149d501681090958
dazuluag <dazuluag@unal.edu.co>
2019-12-19 08:56:01 -0500

    Tarea 01-hadoop-50 q03-10

--- 01-hadoop-50/q03-10/reducer.py
+++ 01-hadoop-50/q03-10/reducer.py
@@ -2,3 +2,7 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line_split = line.split("\t")
+        print("{},{}".format(line_split[1].strip(),line_split[0].strip()))
