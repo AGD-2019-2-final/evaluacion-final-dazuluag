8b67784bfc05bec49dfcbfbf3822166d1bdf7b52
dazuluag <dazuluag@unal.edu.co>
2019-12-19 14:13:34 -0500

    Tarea 01-hadoop-50 q08-10

--- 01-hadoop-50/q08-10/reducer.py
+++ 01-hadoop-50/q08-10/reducer.py
@@ -2,3 +2,17 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    currkey = None
+    total = 0; count = 0
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = float(val)
+        if currkey == key:
+            total += val; count += 1
+        else:
+            if currkey is not None:
+                print("{}\t{}\t{}".format(currkey, total, (total/count)))
+            currkey = key
+            total = val; count = 1
+    print("{}\t{}\t{}".format(currkey, total, (total/count)))
