e78716eef10ce093adba6c488d7bdbfe6bb522ae
dazuluag <dazuluag@unal.edu.co>
2019-12-19 09:20:42 -0500

    Tarea 01-hadoop-50 q04-10

--- 01-hadoop-50/q04-10/reducer.py
+++ 01-hadoop-50/q04-10/reducer.py
@@ -2,4 +2,17 @@ import sys
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
-
+if __name__ == "__main__":
+    currkey = None
+    total = 0
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+        if currkey == key:
+            total += val
+        else:
+            if currkey is not None:
+                print("{},{}".format(currkey, total))
+            currkey = key
+            total = val
+    print("{},{}".format(currkey, total))
