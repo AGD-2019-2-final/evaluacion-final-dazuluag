090e2142931d8a5bff1c43181a45532af718c8af
dazuluag <dazuluag@unal.edu.co>
2019-12-19 15:19:15 -0500

    Tarea 01-hadoop-50 q10-10

--- 01-hadoop-50/q10-10/mapper.py
+++ 01-hadoop-50/q10-10/mapper.py
@@ -2,4 +2,11 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-        
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line_split = line.split("\t")
+        keys_split = line_split[1].strip().split(",")
+        number_str = line_split[0].strip(); number_int = int(number_str)
+        number_str = ("0" + number_str) if number_int < 10 else number_str
+        for key in keys_split:
+            print("{}{}\t{}\t{}".format(key,number_str,key,str(number_int)))
--- 01-hadoop-50/q10-10/reducer.py
+++ 01-hadoop-50/q10-10/reducer.py
@@ -2,3 +2,17 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    currkey = None
+    currnum = ""
+    for line in sys.stdin:
+        tmp, key, val = line.split("\t")
+        val = val.strip()
+        if currkey == key:
+            currnum = "{},{}".format(currnum, val)
+        else:
+            if currkey is not None:
+                print("{}\t{}".format(currkey, currnum))
+            currkey = key
+            currnum = val
+    print("{}\t{}".format(currkey, currnum))
