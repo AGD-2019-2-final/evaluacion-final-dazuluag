bb14cc20f5982ba0d709d91ae08291357fce5e60
dazuluag <dazuluag@unal.edu.co>
2019-12-19 00:28:18 -0500

    Tarea 01-hadoop-50 q02-10

--- 01-hadoop-50/q02-10/mapper.py
+++ 01-hadoop-50/q02-10/mapper.py
@@ -2,3 +2,7 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line_split = line.split(",")
+        print("{}\t{}".format(line_split[3],line_split[4]))
--- 01-hadoop-50/q02-10/reducer.py
+++ 01-hadoop-50/q02-10/reducer.py
@@ -2,3 +2,17 @@ import sys
 #
 #  >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+if __name__ == "__main__":
+    currkey = None
+    currmax = 0
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+        if key == currkey:
+            currmax = val if val > currmax else currmax
+        else:
+            if currkey is not None:
+                print("{}\t{}".format(currkey, currmax))
+            currkey = key
+            currmax = val
+    print("{}\t{}".format(currkey, currmax))
