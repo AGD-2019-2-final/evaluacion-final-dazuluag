0e67af9403037ee4150fde730c41fb09078e6b3b
dazuluag <dazuluag@unal.edu.co>
2019-12-19 13:00:39 -0500

    Tarea 01-hadoop-50 q06-10

--- 01-hadoop-50/q06-10/mapper.py
+++ 01-hadoop-50/q06-10/mapper.py
@@ -2,4 +2,8 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
-
+if __name__ == "__main__":
+    for line in sys.stdin:
+        letter = line.split("   ")[0]
+        value = line.split("   ")[2].strip()
+        print("{}\t{}".format(letter, value))
--- 01-hadoop-50/q06-10/reducer.py
+++ 01-hadoop-50/q06-10/reducer.py
@@ -2,3 +2,19 @@ import sys
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    currkey = None
+    currmin = 0; currmax = 0
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = float(val)
+        if currkey == key:
+            currmin = val if val < currmin else currmin
+            currmax = val if val > currmax else currmax
+        else:
+            if currkey is not None:
+                print("{}\t{}\t{}".format(currkey, currmax, currmin))
+            currkey = key
+            currmin = val
+            currmax = val
+    print("{}\t{}\t{}".format(currkey, currmax, currmin))
