cfdf36a124b4a0ec8aad7866a8e75b83f725b408
dazuluag <dazuluag@unal.edu.co>
2020-02-13 14:32:48 -0500

    Tarea q07 y q09 con grader.py incorrecto

--- 01-hadoop-50/q09-10/mapper.py
+++ 01-hadoop-50/q09-10/mapper.py
@@ -2,3 +2,11 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    for line in sys.stdin:
+        line_split = line.split("   ")
+        number_str = line_split[2].strip(); number_int = int(number_str)
+        number_str = ("0" + number_str) if number_int < 100 else number_str
+        number_str = ("0" + number_str) if number_int < 10 else number_str
+        print("{}\t{}\t{}\t{}".format(
+            number_str, line_split[0],line_split[1],str(number_int)))
--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -2,3 +2,10 @@ import sys
 #
 #  >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+if __name__ == "__main__":
+    count_five = 0
+    for line in sys.stdin:
+        if count_five < 5:
+            count_five += 1
+            line_split = line.split("\t")
+            print("{}   {}   {}".format(line_split[1],line_split[2],line_split[3].strip()))
