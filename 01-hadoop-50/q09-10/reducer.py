19b155490d9f54aa47c2332ef797cad39eba03d2
dazuluag <dazuluag@unal.edu.co>
2020-02-14 17:48:35 -0500

    Corrección grader taller hadoop, punto 07 y 09

--- 01-hadoop-50/q09-10/reducer.py
+++ 01-hadoop-50/q09-10/reducer.py
@@ -5,7 +5,7 @@ import sys
 if __name__ == "__main__":
     count_five = 0
     for line in sys.stdin:
-        if count_five < 5:
+        if count_five < 6:
             count_five += 1
             line_split = line.split("\t")
             print("{}   {}   {}".format(line_split[1],line_split[2],line_split[3].strip()))
